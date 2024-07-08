cc9779da096f45819eacb71bd6936d3609888add
Joaquin Lopez <drwaky@gmail.com>
2024-07-08 00:07:10 -0500

    Adding file for auto format example and another one for variables and type of naming.

---
+++ format.py
@@ -0,0 +1,8 @@
+"""
+Example of a file with code to auto-format
+"""
+
+feeling="happy"
+a        = 13
+b        = 7
+
---
+++ variables.py
@@ -0,0 +1,21 @@
+"""
+Example of variables in Python
+"""
+
+course_name = "Starting with Python"  # Snake case
+CoUrSe_NaMe = "Starting with Python"  # Not recommended
+CourseName = "Starting with Python"  # Pascal case
+courseName = "Starting with Python"  # Camel case
+# course-name = "Starting with Python"  # Kebab case, not valid in Python
+
+COURSE_NAME = "World!"  # Constants are usually in uppercase
+
+# 1name = "John" # Not valid, variable names cannot start with a number
+name1 = "John"
+word = "Hello"
+
+print(course_name, word, COURSE_NAME)
+
+people = 5000
+score = 9.9
+active = True
